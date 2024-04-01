be86fb0bee99cfa4739bfbdce0ba19a10d201773
JaeYeongKim <wodud7308@retgames.co.kr>
2024-04-01 16:06:41 +0900

    공용 패턴 스크립트 추가

---
+++ CastleHero-UnityClient/Assets/Scripts/Common/Pattern/Observer.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace RGLabs.Common.Pattern
+{
+    public class Observer<T> where T : struct
+    {
+        public event Action<T, T> OnChanged;
+        
+        public T It
+        {
+            get => _it;
+            set
+            {
+                T legacy = _it;
+                _it = value;
+                
+                OnChanged?.Invoke(legacy, value);
+            }
+        }
+        
+        private T _it;
+    }
+}
