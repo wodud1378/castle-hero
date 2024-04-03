03865fbc7cc760727da7187ed0c1e0cd4087881c
JaeYeongKim <wodud7308@retgames.co.kr>
2024-04-03 22:55:42 +0900

    필요없는 코드 제거

--- CastleHero-UnityClient/Assets/Scripts/InGame/Behaviours/Wave/SpawnArea.cs
+++ CastleHero-UnityClient/Assets/Scripts/InGame/Behaviours/Wave/SpawnArea.cs
@@ -1,30 +1,17 @@
-using System;
 using System.Collections.Generic;
 using RGLabs.Common;
 using RGLabs.InGame.Behaviours.Unit;
-using RGLabs.InGame.Data.DB;
-using RGLabs.InGame.Data.Model;
 using RGLabs.InGame.System.UnitFactory;
 using RGLabs.InGame.System.Wave;
-using RGLabs.InGame.Utility;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 namespace RGLabs.InGame.Behaviours.Wave
 {
     public class SpawnArea : MonoBehaviour
     {
-        private struct EntitySpace
-        {
-            public int index;
-            public float size;
-        }
-
         [SerializeField] private int _id;
         [SerializeField] private float _size;
-
-        private Queue<CreationRequest[]> _queue;
-
+        
         private CreationHelper _creationHelper;
         private IUnitFactory _factory;
 
@@ -43,11 +30,6 @@ namespace RGLabs.InGame.Behaviours.Wave
                 _factory.PushCreationRequest<MonsterGameUnit>(request.entity,
                     (unit) => { unit.transform.position = request.position; });
             }
-        }
-
-        private void Awake()
-        {
-            _queue = new();
         }
     }
 }
