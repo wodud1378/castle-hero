77cf13f1c5a8b3bb0ceab360e5268500a31a0059
JaeYeongKim <wodud7308@retgames.co.kr>
2024-06-24 14:36:40 +0900

    스킬 빌드 로직 누락된 부분 구현

--- CastleHero-UnityClient/Assets/Scripts/Unit/Skill/SkillHelper.cs
+++ CastleHero-UnityClient/Assets/Scripts/Unit/Skill/SkillHelper.cs
@@ -27,9 +27,14 @@ namespace RGLabs.Unit.Skill
                 case 10011: buildMethod = Build10011; break;
                 case 10013: buildMethod = Build10013; break;
                 case 10016: buildMethod = Build10016; break;
+                case 10019: buildMethod = Build10019; break;
+                case 10020: buildMethod = Build10020; break;
                 case 10021: buildMethod = Build10021; break;
                 case 10023: buildMethod = Build10023; break;
                 case 10025: buildMethod = Build10025; break;
+                case 10031: buildMethod = Build10031; break;
+                case 10032: buildMethod = Build10032; break;
+                case 10033: buildMethod = Build10033; break;
                 case 10034: buildMethod = Build10034; break;
                 default:
                     return null;
@@ -116,6 +121,21 @@ namespace RGLabs.Unit.Skill
                 .SetBound(IDetection.Option.Circle, Targeting.Enemy, 0)
                 .BuildActiveSkill();
         }
+        
+        private static ISkill Build10019(SkillBuilder builder)
+        {
+            return builder
+                .SetTargeting(Targeting.Self, 1)
+                .SetBound(IDetection.Option.Circle, Targeting.Alley, 0)
+                .BuildActiveSkill();
+        }
+        
+        private static ISkill Build10020(SkillBuilder builder)
+        {
+            return builder
+                .SetTargeting(Targeting.Self, 1)
+                .BuildActiveSkill();
+        }
 
         private static ISkill Build10021(SkillBuilder builder)
         {
@@ -137,6 +157,30 @@ namespace RGLabs.Unit.Skill
             return builder
                 .SetTargeting(Targeting.Self, 1)
                 .SetBound(IDetection.Option.Circle, Targeting.Alley, 0)
+                .BuildActiveSkill();
+        }
+        
+        private static ISkill Build10031(SkillBuilder builder)
+        {
+            return builder
+                .SetTargeting(Targeting.Enemy, 1)
+                .SetBound(IDetection.Option.Circle, default, 0)
+                .BuildActiveSkill();
+        }
+        
+        private static ISkill Build10032(SkillBuilder builder)
+        {
+            return builder
+                .SetTargeting(Targeting.Self, 1)
+                .SetBound(IDetection.Option.Circle, Targeting.Enemy, 0)
+                .BuildActiveSkill();
+        }
+        
+        private static ISkill Build10033(SkillBuilder builder)
+        {
+            return builder
+                .SetTargeting(Targeting.Self, 1)
+                .SetBound(IDetection.Option.Circle, Targeting.Enemy, 0)
                 .BuildActiveSkill();
         }
 
