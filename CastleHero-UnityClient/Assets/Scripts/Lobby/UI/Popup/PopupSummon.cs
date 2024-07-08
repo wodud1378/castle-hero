f5867d99a2b8d3fdc610137fdf80423f0840da72
JaeYeongKim <wodud7308@retgames.co.kr>
2024-07-08 16:13:01 +0900

    1. 팝업 애니메이션 루프 해제 2. 소환 팝업 연동

---
+++ CastleHero-UnityClient/Assets/Scripts/Lobby/UI/Popup/PopupSummon.cs
@@ -0,0 +1,11 @@
+using RGLabs.Common.UI.Popup;
+using RGLabs.Utility;
+
+namespace RGLabs.Lobby.UI.Popup
+{
+    [PrefabPath("Lobby/UI/Prefabs/Gacha/Gacha.prefab")]
+    public class PopupSummon : PopupBase
+    {
+        
+    }
+}
